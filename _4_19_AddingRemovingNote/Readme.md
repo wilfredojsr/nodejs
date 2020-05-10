7c6b13c74341ffc3b45e4d807d4ca34040c54203
WISUAREZ-AR\wisuarez <wilfredo.suarez@oracle.com>
2020-05-10 02:04:07 -0300

    _4_19_AddingRemovingNote

---
+++ _4_19_AddingRemovingNote/Readme.md
@@ -0,0 +1,19 @@
+**Commands**
+
+`cd .\_4_19_AddingNote`
+
+Adding a new note
+
+`node .\app.js add --title="Shopping list" --body="An article"`
+
+Adding a new note
+
+`node .\app.js add --title="Shopping" --body="An article"`
+
+Adding a new note
+
+`node .\app.js add --title="Shopping list2" --body="An article"`
+
+Duplicated note
+
+`node .\app.js add --title="Shopping" --body="An article"`
---
+++ _4_19_AddingRemovingNote/app.js
@@ -0,0 +1,61 @@
+const yargs = require('yargs')
+const notes = require('./notes')
+
+console.log('Using process object')
+console.log(process.argv)
+console.log()
+
+console.log('Using Yargs')
+console.log('Customizing the version value')
+yargs.version('1.1.0')
+
+console.log('Creating an add command')
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
+
+console.log('Creating a remove command')
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note')
+  }
+})
+
+console.log('Creating a list command')
+yargs.command({
+  command: 'list',
+  describe: 'Listing your notes',
+  handler: function () {
+    console.log('Listing out all notes')
+  }
+})
+
+console.log('Creating a read command')
+yargs.command({
+  command: 'read',
+  describe: 'Reading the note',
+  handler: function () {
+    console.log('Listing notes')
+  }
+})
+
+yargs.parse()
+//console.log(yargs.argv)
