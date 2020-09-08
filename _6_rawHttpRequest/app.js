fd0a93412e195fc39d82a2a64ac665228a03c7a2
Wilfredo Suarez <wilfredojsr2@gmail.com>
2020-09-07 23:46:32 -0300

    _6_rawHttpRequest

---
+++ _6_rawHttpRequest/app.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const url = 'http://api.weatherstack.com/current?access_key=5581fe424a60423859d2acac8cf51abd&query=40,-75'
+
+const request = http.request(url, (response) => {
+  let data = ''
+console.log('ready')
+  response.on('data', (chunk) => {
+    data = data + chunk.toString()
+  })
+
+  response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body)
+  })
+
+})
+
+request.on('error', (error) => {
+  console.log('An error', error)
+})
+
+request.end()
